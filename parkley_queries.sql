ac4aa2d9e3fa446bc7752a39a67c342a4068e28b
Aman Arya <aman.arya524@gmail.com>
2017-11-05 12:03:47 -0800

    Update parkley_queries.sql
    fixed a few errors in #1 and #2

--- parkley_queries.sql
+++ parkley_queries.sql
@@ -1,19 +1,21 @@
 --1.	Find all the available driveways in Seattle that are under 10$/hour
 SELECT * 
 FROM Driveway 
-WHERE priceRate < 10;
+WHERE priceRate < 10
+AND city = 'Seattle';
 
 --2.	Find all available driveways that operate from 12pm to 4pm.
 SELECT *
 FROM Driveway
-WHERE timein <= '12:00:00' AND timeout >= '16:00:00';
+WHERE timein <= '12:00:00' AND timeout >= '16:00:00'
+AND availible = 1;
 
---3.	Calculate the profit to date for the user ìAmanî, ìAryaî.
+--3.	Calculate the profit to date for the user ‚ÄúAman‚Äù, ‚ÄúArya‚Äù.
 SELECT SUM(totalCost) AS Profit 
 FROM Booking b, Renter r, Users u
 WHERE b.rid = r.rid AND r.uid = u.id AND u.fname = 'Aman' AND u.lname = 'Arya';
 
---4.	Find the driveway for ìLeandro Solidumî which makes him the most money.
+--4.	Find the driveway for ‚ÄúLeandro Solidum‚Äù which makes him the most money.
 SELECT * FROM Driveway D WHERE d.did = (
 	SELECT TOP 1 B.did FROM Booking B 
 	WHERE B.rid = (
@@ -24,20 +26,20 @@ SELECT * FROM Driveway D WHERE d.did = (
 	ORDER BY B.totalcost DESC
 );
 
---5.	Apply a 10% discount on ìAmanî ìAryaî last booking.
+--5.	Apply a 10% discount on ‚ÄúAman‚Äù ‚ÄúArya‚Äù last booking.
 SELECT TOP 1 totalcost*.9 AS 'TotalCost + 10% Discount' FROM Booking B WHERE B.rid = (
 		SELECT r.rid 
 		FROM Renter r, Users u 
 		WHERE r.uid = u.id AND u.fname = 'Aman' AND u.lname = 'Arya'
 ) ORDER BY dt DESC;
 
---6.	Get the phone #ís of the client and user booking made on todayís date(2017-11-05).
+--6.	Get the phone #‚Äôs of the client and user booking made on today‚Äôs date(2017-11-05).
 SELECT u.phone 
 FROM booking b, Client c, users u 
 WHERE b.cid = c.cid AND c.uid = u.id AND b.dt = '2017-11-05'
 ORDER BY b.dt;
 
---7.	Find the price of all driveways that contain ìHarrison Streetî (street of Space Needle) in their address.
+--7.	Find the price of all driveways that contain ‚ÄúHarrison Street‚Äù (street of Space Needle) in their address.
 SELECT priceRate FROM Driveway d
 WHERE d.address LIKE '%Harrison Street%';
 
@@ -56,7 +58,7 @@ ORDER BY Profit DESC;
 --10.	For each driveway find the average amount time in hours that they are booked.
 SELECT avg(DATEDIFF(hour, checkin, checkout)) AS 'avgTimeBooked (hrs)' FROM Booking GROUP BY did;
 
---11.	Find the renter with the most ìavailableî driveway. (Driveway with the most time between time-in and time-out).
+--11.	Find the renter with the most ‚Äúavailable‚Äù driveway. (Driveway with the most time between time-in and time-out).
 SELECT TOP 1 fname, lname FROM Driveway D, Renter R, Users U WHERE D.rid = R.rid AND R.uid = u.id ORDER BY DATEDIFF(hour, timein, timeout) DESC;
 
 --12.	Return how many spots are currently available in the city of Seattle.
